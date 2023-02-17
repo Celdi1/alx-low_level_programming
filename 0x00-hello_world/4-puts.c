69455b02b700c34f50110abfb76cb5ecf44d7999
Celestine Amadi <celestineamadi0852@gmail.com>
2023-02-16 22:23:46 -0800

    Task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: Always 0 (Success)
+ * main - A program that prints a line using printf
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
