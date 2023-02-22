55953ae368e3ade18fc78f3a2beefbf744509532
Celestine Amadi <celestineamadi0852@gmail.com>
2023-02-21 18:16:41 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: addition of both
+ */
+int add(int a, int  b)
+{
+	return (a + b);
+}
