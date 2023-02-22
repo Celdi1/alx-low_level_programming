d1fe7ea096a69f4d94c810f511c29a16db8830d2
Celestine Amadi <celestineamadi0852@gmail.com>
2023-02-22 03:10:25 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,17 @@ void print_to_98(int n)
 
 		while (n < 98)
 		{
-			printf("%i, ", n);
+			printf("%i ", n);
 			n++;
 		}
 		while (n > 98)
 		{
-			printf("%i, ", n);
+			printf("%i ", n);
 			n--;
 		}
 
 		printf("98");
 		putchar('\n');
-			
 }
 
 
