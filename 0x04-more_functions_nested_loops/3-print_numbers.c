321ebc89940cb5ffb2763e73d173da60e3d311ad
Celestine Amadi <celestineamadi0852@gmail.com>
2023-02-23 05:21:25 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9, and then a new line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
