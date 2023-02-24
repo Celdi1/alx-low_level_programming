d079dec8bd6eb2d52d1b87a376fc687ec31c4fd1
Celestine Amadi <celestineamadi0852@gmail.com>
2023-02-23 17:54:34 -0800

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the largest prime number
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (dev < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+		for (dev = 3; dev < (prime / 2); dev += 2)
+		{
+			if ((prime % dev) == 0)
+				prime /= dev;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
