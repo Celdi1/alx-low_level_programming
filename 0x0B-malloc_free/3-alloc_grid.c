52f398f5bdec653491a3763cfa75320068bd1266
Celestine Amadi <celestineamadi0852@gmail.com>
2023-03-14 10:45:01 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,28 +14,28 @@ int **alloc_grid(int width, int height)
 {
 	int **array;
 	int i, j;
-	
+
 	if (height <= 0 || width <= 0)
 		return (NULL);
-	
+
 	array = (int **) malloc(sizeof(int *) * height);
-	
+
 	if (array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			free(array);
-			
+
 			for (j = 0; j <= i; j++)
 				free(array[j]);
 			return (NULL);
 		}
 	}
-	
+
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
@@ -43,6 +43,6 @@ int **alloc_grid(int width, int height)
 			array[i][j] = 0;
 		}
 	}
-	
+
 	return (array);
 }
